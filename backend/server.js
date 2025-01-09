f2e51ea8850b6890570c01e7522b8d1764fa1e10
Philip De-Graft <115908616+kwakuduah12@users.noreply.github.com>
2025-01-09 14:18:47 +0000

    setting up project and dependencies

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log(err));
+
+app.listen(5000, () => {
+  console.log('Server running on port 5000');
+});
